65296044349e2e8e421ead97debb06bc36260dd2
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-10 10:51:46 -0500

    created the feed for the instagram clone

---
+++ photolist.js
@@ -0,0 +1,61 @@
+//put a fake list of photos instead of hitting an api
+
+export const photolist = [
+    {
+        id: 1,
+        photoURL: 'https://images.unsplash.com/photo-1665679293513-9f89c2d006e4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTM3NQ&ixlib=rb-1.2.1&q=80&w=1080',
+        description: 'long shot of two surfers riding a wave',
+        userName: 'surferDude11',
+        profilePhoto: 'https://randomuser.me/api/portraits/men/1.jpg',
+    },
+    {
+        id: 2,
+        photoURL: 'https://images.unsplash.com/photo-1666545585688-1e7f52999476?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTUzNQ&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Long shot of lake and pine forest with foliage covered mountains ',
+        userName: 'Artsy_Guy',
+        profilePhoto: 'https://randomuser.me/api/portraits/men/2.jpg',
+    },
+    {
+        id: 3,
+        photoURL: 'https://images.unsplash.com/photo-1667064486025-36789c0b8101?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU1OQ&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Longshot of a dark modern building with a man walking through empty hallway',
+        userName: 'Haunting_Feel',
+        profilePhoto: 'https://randomuser.me/api/portraits/men/3.jpg',
+    },
+    {
+        id: 4,
+        photoURL: 'https://images.unsplash.com/photo-1667842474031-8a0ba604df48?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU0Mg&ixlib=rb-4.0.3&q=80&w=1080',
+        description: 'Triangles coming together to form hexagon patterns in varying colors',
+        userName: 'Design11',
+        profilePhoto: 'https://randomuser.me/api/portraits/women/4.jpg',
+    },
+    // {
+    //     id: 5,
+    //     photoURL: 'https://images.unsplash.com/photo-1666009373153-2a0b883065b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTU0MA&ixlib=rb-4.0.3&q=80&w=1080',
+    //     description: '',
+    //     userName: '',
+    //     profilePhoto: '',
+    // },
+    // {
+    //     id: 6,
+    //     photoURL: 'https://images.unsplash.com/photo-1666382963735-719eca75c7bd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTUzNw&ixlib=rb-4.0.3&q=80&w=1080',
+    //     description: '',
+    //     userName: '',
+    //     profilePhoto: '',
+    // },
+    // {
+    //     id: 7,
+    //     photoURL: 'https://images.unsplash.com/photo-1666545585688-1e7f52999476?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTUzNQ&ixlib=rb-4.0.3&q=80&w=1080',
+    //     description: '',
+    //     userName: '',
+    //     profilePhoto: '',
+    // },
+    // {
+    //     id: 8,
+    //     photoURL: 'https://images.unsplash.com/photo-1666668973700-e45696248d3f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2ODA5MTUzMg&ixlib=rb-4.0.3&q=80&w=1080',
+    //     description: '',
+    //     userName: '',
+    //     profilePhoto: '',
+    // },
+
+]
